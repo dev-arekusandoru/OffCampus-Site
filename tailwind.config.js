2605d7fa4979b3eda53d8241b2191a0c0e55edb3
Alexandru <a.muresan8533@gmail.com>
2024-02-27 11:28:57 -0500

    begin react-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{html,js}'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			},
 			colors: {
 				error: '#E15554',
 				primary: '#5094F5',
@@ -32,5 +41,5 @@ module.exports = {
 			// => @media (min-width: 1536px) { ... }
 		}
 	},
-	plugins: [require('tailwind-scrollbar-hide')]
+	plugins: []
 };
