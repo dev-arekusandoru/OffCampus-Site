0ea8f263269c19568209b4a07031b10bf54dd98c
Alexandru <a.muresan8533@gmail.com>
2024-02-27 11:40:34 -0500

    set up routes

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => {
+	return (
+		<div>
+			<h1>About Our App</h1>
+			<p>
+				Welcome to our app! This is a brief description of what our app
+				does.
+			</p>
+		</div>
+	);
+};
+
+export default AboutPage;
